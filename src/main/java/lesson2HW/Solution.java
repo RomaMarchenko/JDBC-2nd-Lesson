7f3ef5493632319b962996c47caeba6f4e1d51ae
RomaMarchenko <romamarchenko2001wot@gmail.com>
2020-04-22 15:28:49 +0300

    2nd lesson home task, 2nd commit

--- src/main/java/lesson2HW/Solution.java
+++ src/main/java/lesson2HW/Solution.java
@@ -9,11 +9,22 @@ public class Solution {
     private static final String USER = "main";
     private static final String PASS = "main2001";
 
+    private final String getAllProducts = "SELECT * FROM PRODUCT";
+    private final String getProductsByPrice = "SELECT * FROM PRODUCT WHERE PRICE < 100";
+    private final String getProductsWithDescMoreThan50 = "SELECT * FROM PRODUCT WHERE LENGTH(DESCRIPTION) > 50";
+    private final String getProductsWithDescMoreThan100 = "SELECT * FROM PRODUCT WHERE LENGTH(DESCRIPTION) > 100";
+    private final String increasePrice = "UPDATE PRODUCT SET PRICE = PRICE + 100 WHERE PRICE < 970";
+    private final String updateDescriptionById = "UPDATE PRODUCT SET DESCRIPTION = (?) WHERE ID = (?)";
+
+    private static Connection sqlConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, USER, PASS);
+    }
+
 
     public ArrayList<Product> getAllProducts() {
         ArrayList<Product> products = new ArrayList<>();
-        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
-            try (ResultSet resultSet = statement.executeQuery("SELECT * FROM PRODUCT")) {
+        try (Statement statement = sqlConnection().createStatement()) {
+            try (ResultSet resultSet = statement.executeQuery(getAllProducts)) {
                 while (resultSet.next()) {
                     Product product = new Product(resultSet.getLong("ID"), resultSet.getString("NAME"), resultSet.getString("DESCRIPTION"), resultSet.getInt("PRICE"));
                     products.add(product);
@@ -28,8 +39,8 @@ public class Solution {
 
     public ArrayList<Product> getProductsByPrice() {
         ArrayList<Product> products = new ArrayList<>();
-        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
-            try (ResultSet resultSet = statement.executeQuery("SELECT * FROM PRODUCT WHERE PRICE < 100")) {
+        try (Statement statement = sqlConnection().createStatement()) {
+            try (ResultSet resultSet = statement.executeQuery(getProductsByPrice)) {
                 while (resultSet.next()) {
                     Product product = new Product(resultSet.getLong("ID"), resultSet.getString("NAME"), resultSet.getString("DESCRIPTION"), resultSet.getInt("PRICE"));
                     products.add(product);
@@ -44,12 +55,11 @@ public class Solution {
 
     public ArrayList<Product> getProductsByDescription() {
         ArrayList<Product> products = new ArrayList<>();
-        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
-            try (ResultSet resultSet = statement.executeQuery("SELECT * FROM PRODUCT")) {
+        try (Statement statement = sqlConnection().createStatement()) {
+            try (ResultSet resultSet = statement.executeQuery(getProductsWithDescMoreThan50)) {
                 while (resultSet.next()) {
                     Product product = new Product(resultSet.getLong("ID"), resultSet.getString("NAME"), resultSet.getString("DESCRIPTION"), resultSet.getInt("PRICE"));
-                    if (product.getDescription().length() > 50)
-                        products.add(product);
+                    products.add(product);
                 }
             }
         } catch (SQLException e) {
@@ -60,8 +70,8 @@ public class Solution {
     }
 
     public void increasePrice() {
-        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
-            statement.executeUpdate("UPDATE PRODUCT SET PRICE = PRICE + 100 WHERE PRICE < 970");
+        try (Statement statement = sqlConnection().createStatement()) {
+            statement.executeUpdate(increasePrice);
         } catch (SQLException e) {
             System.err.println("Something went wrong");
             e.printStackTrace();
@@ -69,29 +79,27 @@ public class Solution {
     }
 
     public void changeDescription() {
-        try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
-            try (ResultSet resultSet = statement.executeQuery("SELECT * FROM PRODUCT")) {
+        try (Statement statement = sqlConnection().createStatement()) {
+            try (ResultSet resultSet = statement.executeQuery(getProductsWithDescMoreThan100)) {
                 while (resultSet.next()) {
                     Product product = new Product(resultSet.getLong("ID"), resultSet.getString("NAME"), resultSet.getString("DESCRIPTION"), resultSet.getInt("PRICE"));
-                    if (product.getDescription().length() > 100) {
-                        String[] allSentences = product.getDescription().split("\\. ");
-                        StringBuilder newDescription = new StringBuilder();
-                        for(int index = 0; index < allSentences.length - 1; index++) {
-                            newDescription.append(allSentences[index]).append(". ");
-                        }
-                        newDescription = new StringBuilder(newDescription.toString().trim());
-                        long productId = product.getId();
-                        try(PreparedStatement preparedStatement = connection.prepareStatement("UPDATE PRODUCT SET DESCRIPTION = (?) WHERE ID = (?)")) {
-                            preparedStatement.setString(1, String.valueOf(newDescription).trim());
-                            preparedStatement.setLong(2, productId);
-                            preparedStatement.executeUpdate();
-                        }
+                    String[] allSentences = product.getDescription().split("\\. ");
+                    StringBuilder newDescription = new StringBuilder();
+                    for (int index = 0; index < allSentences.length - 1; index++) {
+                        newDescription.append(allSentences[index]).append(". ");
+                    }
+                    newDescription = new StringBuilder(newDescription.toString().trim());
+                    long productId = product.getId();
+                    try (PreparedStatement preparedStatement = sqlConnection().prepareStatement(updateDescriptionById)) {
+                        preparedStatement.setString(1, String.valueOf(newDescription).trim());
+                        preparedStatement.setLong(2, productId);
+                        preparedStatement.executeUpdate();
                     }
                 }
             }
-            } catch (SQLException e) {
-                System.err.println("Something went wrong");
-                e.printStackTrace();
-            }
+        } catch (SQLException e) {
+            System.err.println("Something went wrong");
+            e.printStackTrace();
+        }
     }
 }
